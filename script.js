a584d5b59e99ba802a054dbcce58073f1b532587
Luiz Henrique <ludehenriq@gmail.com>
2023-07-24 14:28:54 -0300

    Alteraçoes-avatar-cores

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function ToggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute("src", "./assets-luiz/avatar-luiz-light.jpg")
   } else {
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute("src", "./assets-luiz/avatar-luiz.jpg")
   }
 }
